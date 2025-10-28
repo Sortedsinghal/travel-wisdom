481671f45867e46487707097331a6717a9756e13
Sortedsinghal <Krishnasinghal2005@icloud.com>
2025-10-29 03:02:19 +0530

    Fix nodemailer transport creation in send-query API

--- api/send-query.js
+++ api/send-query.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
   const { name, email, phone, message, tripName } = req.body;
   
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER || 'Krishnasinghal9711@gmail.com',
